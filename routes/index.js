f507759b57998cac4223e1c8727cc3a2de9b9556
Shuyang <liushuyang2009@gmail.com>
2015-12-30 22:58:01 -0500

    fix the menu bar on chrome

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/agreement', function(req, res){
+  
+});
+
 router.get('/home',function(req, res) {
   fs.readdir('./Files', function(err, files){
     if (err) {
@@ -27,6 +31,7 @@ router.get('/about',function(req, res){
     {'files' : files});
   });
 });
+
 
 router.get('/contacts',function(req, res){
   fs.readdir('./Files', function(err, files){
@@ -52,23 +57,39 @@ router.get('/reports',function(req, res){
 
     res.render('reports.html',
     {'files' : files,
-      'folders': folders});
+     'folders': folders});
   });
 });
 
-router.get('/reports/*',function(req,res){
-  var filename = './Files/'+req.params[0];
+router.get('/reports/*/*', function(req, res){
+  //render the report page for a specific year
+  var year = req.params[0];
+  var file = req.params[1];
+  
+  var filename = './Files/'+year+ '/' +file;
   fs.readFile(filename, function (err,data){
      res.contentType("application/pdf");
      res.send(data);
   });
-});
-
-router.get('/r/*', function(req, res){
-  //render the report page for a specific year
-  var year = req.params[0];
   
 });
+
+router.get('/reports/*',function(req,res){
+  var year = req.params[0];
+  fs.readdir('./Files', function(err, files){
+    if (err) {
+      throw err;
+    }
+    //files in the specific folder
+    var file = fs.readdirSync('./Files/'+year);
+    res.render('year.html',
+    {'files' : files,
+      'year' : year,
+      'file': file});
+  });
+});
+
+
 
 router.get('/invest',function(req, res){
   fs.readdir('./Files', function(err, files){
